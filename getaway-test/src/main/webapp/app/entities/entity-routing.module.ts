4205bdbc67d3815c32de1efd4e8d59a880a34831
drasko-kosovic <drasko-kosovic@gmail.com>
2021-12-12 09:26:52 +0100

    init

--- getaway-test/src/main/webapp/app/entities/entity-routing.module.ts
+++ getaway-test/src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,6 +4,11 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'proba',
+        data: { pageTitle: 'Probas' },
+        loadChildren: () => import('./servis/proba/proba.module').then(m => m.ServisProbaModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
